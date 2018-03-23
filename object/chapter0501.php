32af63618a75049f84f6d464ea2ed505dc79d50d
阿伟 <awei@junapi.com>
2018-03-23 18:42:30 +0800

    深入php面向对象模式与实践 04 05 源码

--- object/chapter0501.php
+++ object/chapter0501.php
@@ -9,22 +9,35 @@
 namespace tasks;
 
 class Task{
+    public $ch = '中文';
+    
     function doSpeak(){
         print "hello\n";
     }
 }
 
 $classname = "Task";
-if(!file_exists("tasks/{$classname}.php")){    
-    throw new \Exception("no such file as tasks/{$classname}.php");
-}
-
-require_once "tasks/{$classname}.php";
+//if(!file_exists("tasks/{$classname}.php")){    
+//    throw new \Exception("no such file as tasks/{$classname}.php");
+//}
+//
+//require_once "tasks/{$classname}.php";
 
 $classname = "tasks\\".$classname;
 if(!class_exists($classname)){
     throw new \Exception("no such class as {$classname}");
 }
 
+//print_r(get_declared_classes());
+
 $myObj = new $classname();
 $myObj->doSpeak();
+
+print_r(get_class($myObj));
+
+if($myObj instanceOf Task){
+    print " {$product} is a Task object\n";
+}
+
+print_r(get_class_methods($myObj));
+print_r(get_class_vars('Task'));
